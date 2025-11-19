984580057d56780f8c75572101a9e9c8cc638056
dhavalzindex <dhaval.zindexsolutions@gmail.com>
2025-11-19 16:30:59 +0530

    file name make capital 1214

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./assets/fonts/fonts.css";
 import "./App.css";
-import Signup from "./pages/Signup";
-import VerifyAddress from "./pages/VerifyAddress";
-import EmailConfirmation from "./pages/EmailConfirmation";
-import CreateAccount from "./pages/CreateAccount";
+import Signup from "/src/pages/Signup";
+import VerifyAddress from "/src/pages/VerifyAddress";
+import EmailConfirmation from "/src/pages/EmailConfirmation";
+import CreateAccount from "/src/pages/CreateAccount";
 
 function App() {
   return (
