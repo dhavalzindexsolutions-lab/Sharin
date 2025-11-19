d3bdf9251eb0eaa9f796603ba94e7f7027d13bf8
dhavalzindex <dhaval.zindexsolutions@gmail.com>
2025-11-19 16:17:57 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
+import { reactRouter } from "@react-router/dev/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(),reactRouter(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
