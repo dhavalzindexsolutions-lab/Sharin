cee966c43ffbe4b85d49c775ff9d81a9a5d8ece6
dhavalzindex <dhaval.zindexsolutions@gmail.com>
2025-11-19 15:35:58 +0530

    footer updated

--- src/molecules/Footer/FooterLinks.jsx
+++ src/molecules/Footer/FooterLinks.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router';
+import React from "react";
+import { Link } from "react-router";
 
 const FooterLinks = () => {
   const currentYear = new Date().getFullYear();
@@ -10,25 +10,24 @@ const FooterLinks = () => {
     { label: "Norme sui Cookie", to: "#" },
     { label: `© ${currentYear} Sharin Srl` }, // plain text, no link
   ];
-  return (
-    <>
-      <div className="block p-4 lg:p-6">
-        <ul className="flex flex-wrap items-center justify-center text-center text-sm md:text-16 gap-y-2 gap-x-4 text-[#767676]">
-          {items.map((item, index) => (
-            <li key={index}>
-              {item.to ? (
-                <Link to={item.to} className="hover:text-black">
-                  {item.label}
-                </Link>
-              ) : (
-                <span>{item.label}</span>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  )
-}
 
-export default FooterLinks
+  return (
+    <div className="block p-4 lg:p-6">
+      <ul className="flex flex-wrap items-center justify-center text-center text-sm md:text-16 gap-y-2 gap-x-4 text-[#767676]">
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.to ? (
+              <Link to={item.to} className="hover:text-black">
+                {item.label}
+              </Link>
+            ) : (
+              <span>{item.label}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FooterLinks;
