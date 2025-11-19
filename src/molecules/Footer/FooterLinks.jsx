d0a1a4cb96c6f09efcb7800a1d3d3c4e23c59d3a
dhavalzindex <dhaval.zindexsolutions@gmail.com>
2025-11-19 15:26:00 +0530

    footer file updated

--- src/molecules/Footer/FooterLinks.jsx
+++ src/molecules/Footer/FooterLinks.jsx
@@ -11,21 +11,23 @@ const FooterLinks = () => {
     { label: `© ${currentYear} Sharin Srl` }, // plain text, no link
   ];
   return (
-    <div className="block p-4 lg:p-6">
-      <ul className="flex flex-wrap items-center justify-center text-center text-sm md:text-16 gap-y-2 gap-x-4 text-[#767676]">
-        {items.map((item, index) => (
-          <li key={index}>
-            {item.to ? (
-              <Link to={item.to} className="hover:text-black">
-                {item.label}
-              </Link>
-            ) : (
-              <span>{item.label}</span>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="block p-4 lg:p-6">
+        <ul className="flex flex-wrap items-center justify-center text-center text-sm md:text-16 gap-y-2 gap-x-4 text-[#767676]">
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.to ? (
+                <Link to={item.to} className="hover:text-black">
+                  {item.label}
+                </Link>
+              ) : (
+                <span>{item.label}</span>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 
